ec65570837332971957962072abdd4c6fb7ffc40
lunabeta <lunaworku@gmail.com>
2025-11-03 20:16:19 +0300

    vite and netlify setup

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist",   
+    outDir: "dist",
     emptyOutDir: true
-  },
-  server: {
-    port: 5173
   }
 });
